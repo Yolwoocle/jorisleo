1301b4e26ba0c031e189912d16a9c262c5afeb92
Skapdat <skapdaht@gmail.com>
2019-08-15 20:29:42 +0200

    Gestion click cactus

--- client/main.js
+++ client/main.js
@@ -19,12 +19,15 @@ var config = {
 
 var gameOptions = {
     jumpVelocity: 700,
-    platformSpeed: 500,
-    playerStartPosition: 100
+    platformSpeed: 200,
+    playerStartPosition: 100,
+    jumpNumber:3,
+    cactusLimit: 800
 }
 
-var platforms, cursors, player, map, groundLayer, obstacleGroup;
+var platforms, cursors, player, map, groundLayer, obstacleGroup, groundLayer2;
 var cactus;
+var canCactus = true;
 var keys = {};
 var pointer
 
@@ -50,47 +53,71 @@ function create ()
       Phaser.Actions.PlaceOnLine(this.groundLayer.getChildren(),
         new Phaser.Geom.Line(10, 590, 810, 590));
       this.groundLayer.refresh();
+    groundLayer2 = this.add.tileSprite(400,590,800,20, 'ground')
 
     player = this.physics.add.sprite(gameOptions.playerStartPosition, 450, 'player');
     player.displayWidth = 50;
     player.displayHeight = 50;
     player.setCollideWorldBounds(true);
     player.body.setGravityY(config.physics.arcade.gravity.y);
+    player.jumps = 0;
     this.physics.add.collider(player, this.groundLayer);
 
+    this.jump = function () {
+        if (player.body.touching.down) {
+            player.jumps = 0;
+        }
+        if (player.jumps < gameOptions.jumpNumber) {
+            player.jumps++;
+            player.setVelocityY(-gameOptions.jumpVelocity);
+        }
+    }
+
     this.addCactus = function(posX) {
-        cactus = this.physics.add.sprite(posX, 0, 'cactusS1')
-        cactus.setCollideWorldBounds(true);
+        let catT = ['cactusS1', 'cactusS2', 'cactusB1', 'cactusB2'];
+        cactus = this.physics.add.sprite(posX, 0, catT[Math.floor(Math.random() * 4)])
         cactus.body.setGravityY(config.physics.arcade.gravity.y);
         cactus.setBounce(Math.random());
         this.physics.add.collider(cactus, this.groundLayer);
-        cactus.setVelocityX(gameOptions.platformSpeed * -1)
+        this.physics.add.collider(cactus, groundLayer2);
+        cactus.hasTouched = false;
+        
+        this.physics.add.overlap(player, cactus, function(cldPlayer, cldCactus) {
+            if (!cldCactus.hasTouched) {
+                cldCactus.hasTouched = true;
+                console.log('Touché');
+            }
+        }, null, this);
     }
-
-    
-    this.physics.add.overlap(player, cactus, function() {
-        console.log('Touché')
-    }, null, this);
     
     pointer = this.input.activePointer;
-    this.input.on('pointerup', function(pointer){
-        scene.addCactus(750);
+    this.input.on('pointerdown', function(pointer){
+        if (canCactus) {
+            scene.addCactus((Math.random() * 200) + 600);
+            canCactus = false;
+            setTimeout(function() {
+                canCactus = true;
+            }, gameOptions.cactusLimit)
+        }
     })
 
     keys.UP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     keys.DOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN); 
 
+    this.input.keyboard.on('keydown_UP', function() {
+        scene.jump();
+    });
+    
 }
 
 function update ()
 {
     //PLAYER
     player.x = gameOptions.playerStartPosition;
-
-    if (player.body.touching.down && keys.UP.isDown)
-    {
-        player.setVelocityY(-gameOptions.jumpVelocity);
+    if (groundLayer2) { 
+        groundLayer2.tilePositionX += 3
     }
+
     if (keys.DOWN.isDown)
     {
         player.body.setGravityY(10000);
@@ -98,6 +125,10 @@ function update ()
     if (keys.DOWN.isUp)
     {
         player.body.setGravityY(config.physics.arcade.gravity.y);
+    }
+
+    if (cactus && cactus.body.touching.down) {
+        cactus.setVelocityX(gameOptions.platformSpeed * -1)
     }
 
     //CACTUS
