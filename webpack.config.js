3cd1f56707c18a9d112d839a7dc2e39b16b95d4c
Skapdat <skapdaht@gmail.com>
2019-08-21 12:40:03 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '/')
-  }
+    mode: 'development',
+    entry: './client/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
